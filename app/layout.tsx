c2c9c11da623469c89d92203a33105a695561a16
nicolas-angelo <57606004+nicolas-angelo@users.noreply.github.com>
2023-03-31 17:07:27 -0400

    added tailwind config, app dir and route handlers

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import './global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="h-screen bg-neutral-800">{children}</body>
+    </html>
+  );
+}
