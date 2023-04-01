265c9c3d95567be3b87a352e88ba92a7d8482721
nicolas-angelo <57606004+nicolas-angelo@users.noreply.github.com>
2023-04-01 08:26:09 -0400

    updated settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className="h-screen bg-neutral-800">{children}</body>
+			<body className="h-screen flex flex-wrap">{children}</body>
 		</html>
 	);
 }
