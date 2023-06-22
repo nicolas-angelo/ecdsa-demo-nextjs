4bcb72c16db2117df1019e29975cd8a58d9c0931
nicolas-angelo <nick@stealthist.io>
2023-06-22 09:19:33 -0400

    ai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,38 @@
+'use client';
+import {
+	SandpackProvider,
+	SandpackLayout,
+	SandpackCodeEditor,
+	SandpackFileExplorer,
+	SandpackPreview,
+} from '@codesandbox/sandpack-react';
+import { autocompletion, completionKeymap } from '@codemirror/autocomplete';
+import {
+	cyberpunk,
+	amethyst,
+	nightOwl,
+	freeCodeCampDark,
+} from '@codesandbox/sandpack-themes';
+
 export default async function Home() {
 	return (
 		<div className="flex min-h-full flex-col py-12 sm:px-6 lg:px-8">
-			<div className="sm:mx-auto sm:w-full sm:max-w-md">hello</div>
+			<SandpackProvider template="react" theme={amethyst}>
+				<SandpackLayout>
+					<SandpackFileExplorer />
+					<SandpackCodeEditor
+						showLineNumbers={true}
+						showInlineErrors
+						wrapContent
+						extensions={[autocompletion()]}
+						//@ts-ignore
+						extensionsKeymap={[completionKeymap]}
+					/>
+					<div className="sm:mx-auto sm:w-full sm:max-w-md">
+						<p className="font-bold text-white">hello</p>
+					</div>
+				</SandpackLayout>
+			</SandpackProvider>
 		</div>
 	);
 }
