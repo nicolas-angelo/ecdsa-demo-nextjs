3319219e9283e41b7bbde205f8c2b0f914fbd488
nicolas-angelo <nick@stealthist.io>
2023-06-22 09:21:45 -0400

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // import { getServerSession } from "next-auth/next";
-import Providers from "./providers";
+import Providers from './providers';
 // import { authOptions } from "app/pages/api/auth/[...nextauth]";
-import "app/app/global.css";
+import 'app/app/global.css';
 
 export default async function RootLayout({
 	children,
